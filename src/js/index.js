66ba9d1bd3e99c739b6a5470ba99e1dc8c028533
dimovich85 <dimovich85@gmail.com>
2020-09-01 17:50:29 +0300

    preloader

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,22 @@ let wow = new WOW({
     animateClass: 'animate__animated'
 });
 wow.init();
+
+let preloader = document.querySelector('.preloader');
+preloader.addEventListener('animationend', function(e){
+    if( e.target === preloader ){
+        document.body.classList.remove('ov-h');
+    }
+});
+
+// let objs = [
+//     '.cup',
+//     '.steam',
+//     '.sleeve',
+//     '.lid'
+// ];
+
+// objs.forEach(function(elem){
+//     let pathLen = document.querySelector(elem).getTotalLength();
+//     console.log(elem + ': ' + pathLen);
+// });
